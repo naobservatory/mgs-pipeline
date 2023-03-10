accb753133c8ebd96cd43f694598e967da76188b
Jeff Kaufman <jeff@securebio.org>
2023-03-10 20:39:13 +0000

    import-accession: copy all associated study files from ENA to s3://nao-mgs/STUDY/raw/

---
+++ import-accession.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+if [ $# -lt 1 ]; then
+    echo "Usage: ./import-accession.sh <accessions>"
+fi
+
+for ACCESSION in "$@"; do 
+    ENDPOINT="https://www.ebi.ac.uk/ena/portal/api/filereport"
+    FIELDS="fastq_ftp"
+    PARAMS="?accession=${ACCESSION}&fields=${FIELDS}&format=tsv"
+    PARAMS="${PARAMS}&result=read_run&download=true&limit=0"
+
+    curl -sS "${ENDPOINT}${PARAMS}" | \
+        grep -v fastq_ftp | \
+        awk '{print $NF}' | \
+        tr ';' '\n' | \
+        xargs -P 4 -I {} bash -c \
+              'curl -sS {} | \
+               aws s3 cp - s3://nao-mgs/'$ACCESSION'/raw/$(basename {})'
+done
