662f243eb5183606eeff2829f6e976633612aacb
Jeff Kaufman <jeff@securebio.org>
2023-03-13 13:13:52 +0000

    on re-run only import files that aren't already in s3

--- import-accession.sh
+++ import-accession.sh
@@ -4,7 +4,7 @@ if [ $# -lt 1 ]; then
     echo "Usage: ./import-accession.sh <accessions>"
 fi
 
-for ACCESSION in "$@"; do 
+for ACCESSION in "$@"; do
     ENDPOINT="https://www.ebi.ac.uk/ena/portal/api/filereport"
     FIELDS="fastq_ftp"
     PARAMS="?accession=${ACCESSION}&fields=${FIELDS}&format=tsv"
@@ -15,6 +15,9 @@ for ACCESSION in "$@"; do
         awk '{print $NF}' | \
         tr ';' '\n' | \
         xargs -P 4 -I {} bash -c \
-              'curl -sS {} | \
-               aws s3 cp - s3://nao-mgs/'$ACCESSION'/raw/$(basename {})'
+          'if ! aws s3 ls s3://nao-mgs/'$ACCESSION'/raw/$(basename {}) \
+               &>/dev/null; then \
+             curl -sS {} | \
+               aws s3 cp - s3://nao-mgs/'$ACCESSION'/raw/$(basename {}); \
+           fi'
 done
