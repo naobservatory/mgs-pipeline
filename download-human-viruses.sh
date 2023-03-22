a73b3172d963cdecfe461037c9160c6a7c317cfa
Jeff Kaufman <jeff@securebio.org>
2023-03-22 19:27:59 +0000

    download-human-viruses: better docs

--- download-human-viruses.sh
+++ download-human-viruses.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-# ncbi taxid TAB scientific name
+# TSV format is "ncbi taxid \t scientific name"
+# 9606 is the taxid for humans
 curl -sS https://www.genome.jp/ftp/db/virushostdb/virushostdb.tsv \
     | awk -F'\t' '$8=="9606"{print $1"\t"$2}' \
     | sort -n \
