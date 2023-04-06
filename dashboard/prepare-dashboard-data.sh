675ce7210a8c41c882cbec22257c432141e0248e
Jeff Kaufman <jeff@securebio.org>
2023-04-06 18:51:20 +0000

    dashboard: always prepare data in dashboard/ dir

--- dashboard/prepare-dashboard-data.sh
+++ dashboard/prepare-dashboard-data.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# always run in dashboard dir
+cd -- "$( dirname -- "${BASH_SOURCE[0]}")"
+
 if [ ! -e names.dmp ] ; then
     wget https://ftp.ncbi.nlm.nih.gov/pub/taxonomy/taxdmp.zip
     unzip taxdmp.zip
