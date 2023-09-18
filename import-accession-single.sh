a1937c1b14b2e125e977576bf54fc06baaa732c4
Jeff Kaufman <jeff@securebio.org>
2023-09-18 13:37:05 +0000

    reversed conditions

--- import-accession-single.sh
+++ import-accession-single.sh
@@ -7,8 +7,8 @@ BASE=$(basename "$URL")
 if aws s3 ls "s3://nao-mgs/$BIOPROJECT/raw/$BASE" | \
         awk '$3>0{print}' | \
         grep "$BASE" > /dev/null; then
+    echo "$BASE already present"
+else
     echo "Downloading $BASE"
     curl -sS "$URL" | aws s3 cp - "s3://nao-mgs/$BIOPROJECT/raw/$BASE"
-else
-    echo "$BASE already present"
 fi
