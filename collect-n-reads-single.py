621554d45629a4c44e574ecc326c7df7e048b9a2
Jeff Kaufman <jeff@securebio.org>
2023-09-20 01:53:03 +0000

    Fix read counting
    Grepping for fastq lines starting with @ is very fast but isn't reliable; switch to a real parser.
    
    Fixes #18

---
+++ collect-n-reads-single.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import sys
+from Bio.SeqIO.QualityIO import FastqGeneralIterator
+
+reads = 0
+for (title, sequence, quality) in FastqGeneralIterator(sys.stdin):
+    reads += 1
+
+print(reads)
