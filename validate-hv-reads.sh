a6210ec67b311cfe85be77e7186fde765fdde158
simonleandergrimm <simonleandergrimm@gmail.com>
2023-11-14 16:16:01 -0500

    Updated code to use newest version of return_alignment_scores.py

--- validate-hv-reads.sh
+++ validate-hv-reads.sh
@@ -6,6 +6,25 @@ ROOT_DIR="$PWD"
 DATA_DIR="$ROOT_DIR/validation-test"
 BOWTIE_DIR="$ROOT_DIR/.." #or just add bowtie2 to your path
 
+#assumes that hv_reads files are present 
+
+if [ ! -d $DATA_DIR/hvfastqs ]; then
+    mkdir $DATA_DIR/hvfastqs
+    ls $DATA_DIR/hvreads | \
+        xargs -P 32 -I {} \
+	$ROOT_DIR/json_to_fasta.py $DATA_DIR/hvreads $DATA_DIR/hvfastqs {}
+fi
+
+
+$ROOT_DIR/get_genomes.py
+
+if [ ! -d $ROOT_DIR/raw-genomes ]; then
+    mkdir $ROOT_DIR/raw-genomes
+    for x in $(find $ROOT_DIR/refseq/ | grep gz$); do
+        gunzip -c "$x" > $ROOT_DIR/raw-genomes/$(basename ${x/.fna.gz/.fna})
+    done
+fi
+
 if [ ! -e $DATA_DIR/all_references.fna ]; then
     find raw-genomes | grep .fna$ | xargs cat > $DATA_DIR/all_references.fna
 fi
@@ -23,11 +42,12 @@ fi
 if [ ! -e $DATA_DIR/hvsams ]; then
     mkdir $DATA_DIR/hvsams
     $ROOT_DIR/run_bowtie2.py \
-	$BOWTIE_DIR
+	$BOWTIE_DIR \
+	$DATA_DIR
 fi
 
 if [ ! -e alignment_scores.tsv ]; then
-    $ROOT_DIR/alignment_scores.py \
+    $ROOT_DIR/return_alignment_scores.py \
 	$ROOT_DIR/hvsams \
 	alignment_scores.tsv
 fi
