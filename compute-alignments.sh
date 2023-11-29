5692c08445dd3286efa9027593e6e0f66ac2179f
Jeff Kaufman <jeff@jefftk.com>
2023-11-29 08:36:24 -0500

    alignments: align all reads, not just hvreads (#47)
    Some HV reads are not identified by Kraken, so cast a wider net by running Bowtie across all cleaned reads.
    
    We won't use all of these: some Kraken correctly can tell are Human etc which Bowtie (with the HV-only DB we're using) has no insight into, and others have such low alignment scores that they may as well be junk (but we haven't yet decided which score to use as a cutoff).  They're small enough, though, that no harm in keeping them.

---
+++ compute-alignments.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# I wish I could do this in pure python, which would have more robust error
+# handling, but I'm not sure how to write this kind of branching command tree
+# with the subprocess module.
+
+set -e
+set +x
+
+CMD="/home/ec2-user/bowtie2-2.5.2-linux-x86_64/bowtie2"
+CMD+=" --local"
+CMD+=" -x /home/ec2-user/mgs-pipeline/bowtie/human-viruses"
+CMD+=" --threads 8"
+CMD+=" --very-sensitive-local"
+CMD+=" --score-min G,1,0"
+CMD+=" --mp 4,1"
+CMD+=" --no-unal"
+CMD+=" --no-sq"
+CMD+=" -S $1"
+
+if [ $# = 2 ]; then
+    $CMD -U <(aws s3 cp $2 - | gunzip)
+elif [ $# = 3 ]; then
+    $CMD -1 <(aws s3 cp $2 - | gunzip) -2 <(aws s3 cp $3 - | gunzip)
+else
+    >&2 echo "usage: $0 <sam_out> <fastq_in> [fastq2_in]"
+fi
