75d29fae461605b5d5a1603fa26e2827eb2dd1b9
Jeff Kaufman <jeff@jefftk.com>
2023-12-11 16:55:53 -0500

    add no-bact-no-human-hv-bowtie.py

--- classifier-comparision/evaluate.py
+++ classifier-comparision/evaluate.py
@@ -2,12 +2,31 @@
 
 import os
 import sys
+import glob
 import gzip
 import json
 import importlib
 from collections import Counter, defaultdict
 
 MGS_PIPELINE_DIR = os.path.join(os.path.dirname(__file__), "..")
+DASHBOARD_DIR = os.path.join(MGS_PIPELINE_DIR, "dashboard")
+
+parents = {}
+with open(os.path.join(DASHBOARD_DIR, "nodes.dmp")) as inf:
+    for line in inf:
+        child_taxid, parent_taxid, *_ = \
+            line.replace("\t|\n", "").split("\t|\t")
+        parents[int(child_taxid)] = int(parent_taxid)
+
+def taxid_under(clade, taxid):
+    while taxid not in [0, 1]:
+        if taxid == clade:
+            return True
+        taxid = parents[taxid]
+    return False
+
+def is_bacterial(taxid):
+    return taxid_under(2, taxid)
 
 def collect_classification_data(read_ids):
     cdata = {}
@@ -19,8 +38,7 @@ def collect_classification_data(read_ids):
             human_viruses[int(taxid)] = name
 
     cdata["human_viruses"] = human_viruses
-    cdata["dashboard_dir"] = os.path.join(
-        MGS_PIPELINE_DIR, "dashboard")
+    cdata["dashboard_dir"] = DASHBOARD_DIR
 
     sample_ids = set(read_id_to_sample_id(read_id)
                      for read_id in read_ids)
@@ -30,6 +48,7 @@ def collect_classification_data(read_ids):
         "hv": defaultdict(list),
         "human": defaultdict(list),
     }
+    cdata["processed"] = {}
     for sample in sample_ids:
         with open(os.path.join(cdata["dashboard_dir"],
                                "hvreads",
@@ -51,6 +70,18 @@ def collect_classification_data(read_ids):
                     if read_id in read_ids:
                         cdata["alignments"][bowtie_db][read_id].append(bits)
 
+        for fname in glob.glob(
+                os.path.join(cdata["dashboard_dir"],
+                             "processed", "*.kraken2.tsv.gz")):
+            with gzip.open(fname, "rt") as inf:
+                for line in inf:
+                    bits = line.removesuffix("\n").split("\t")
+                    read_id = bits[1]
+                    if read_id in read_ids:
+                        cdata["processed"][read_id] = bits
+
+    cdata["is_bacterial"] = is_bacterial
+
     return cdata
 
 def read_id_to_sample_id(read_id):
@@ -65,10 +96,13 @@ def start(*classifier_names):
         ground_truth = json.load(inf)
 
     all_read_ids = set(ground_truth["yes"]) | set(ground_truth["no"])
+    #all_read_ids = [x for x in all_read_ids if "SRR14530891" in x]
 
     cdata = collect_classification_data(all_read_ids)
-    
+
     metrics = [Counter() for _ in classifiers]
+
+    metric_names = set()
 
     print("read_id", *classifier_names, sep="\t")
     for read_id in sorted(all_read_ids):
@@ -85,12 +119,13 @@ def start(*classifier_names):
                 (False, True): "falsepos",
                 (True, False): "falseneg",
             }[is_hv, result]
+            metric_names.add(status)
             metric[status] += 1
 
     classifier_column_width = max(
         len(classifier_name)
         for classifier_name in classifier_names)
-    metric_names = list(sorted(metrics[0]))
+    metric_names = list(sorted(metric_names))
     print(" "*classifier_column_width,
           *metric_names,
           sep="  ")
---
+++ classifier-comparision/no-bact-no-human-hv-bowtie.py
@@ -0,0 +1,56 @@
+import re
+import os
+import json
+
+def classify(sample, read_id, cdata):
+    human_al = cdata["alignments"]["human"].get(read_id, None)
+    if human_al:
+        return False
+
+    kraken_result = cdata["processed"].get(read_id, None)
+    if kraken_result:
+        taxid, = re.findall(".*taxid ([0-9]+).*", kraken_result[2])
+        taxid = int(taxid)
+
+        if taxid == 9606:  # Kraken assigned human
+            return False
+        if cdata["is_bacterial"](taxid):
+            return False
+    
+    hv_al = cdata["alignments"]["hv"].get(read_id, None)
+    if not hv_al:
+        return False
+
+    score = 0
+    length = 0
+    for read_part in hv_al:
+        _, _, _, _, _, part_score, part_length = read_part
+        score += int(part_score)
+        length += int(part_length)
+
+    bend1_length = 40
+    bend1_score = 60
+
+    bend2_length = 80
+    bend2_score = 95
+
+    max_length = 200
+    max_score = 100
+
+    min_length_cutoff = 28
+    uncollapsed_score = 269
+
+    if length < min_length_cutoff:
+        return False
+    elif not read_id.startswith("M_"):
+        return score >= uncollapsed_score
+    elif length < bend1_length:
+        return score >= bend1_score
+    elif length < bend2_length:
+        return score >= (
+            (length - bend1_length) / (bend2_length - bend1_length)
+            * (bend2_score - bend1_score)) + bend1_score
+    else:
+        return score >= (
+            (length - bend2_length) / (max_length - bend2_length)
+            * (max_score - bend2_score)) + bend2_score
