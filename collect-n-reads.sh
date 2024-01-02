naobservatory/mgs-pipeline

commit 2e34c208c524890fc995232b8e067a5a620e09dc

Read counting: ignored bioprojects that exist in S3 but not on disk
This is happening because https://github.com/naobservatory/mgs-pipeline/pull/48 hasn't been merged yet, and will always happen when someone is in the process of importing a paper.

collect-n-reads.sh
@@ -12,6 +12,9 @@ else
 fi
 
 for bioproject in $(aws s3 ls $S3_DIR | awk '{print $NF}'); do
+    if [ ! -d ${bioproject}metadata/ ] ; then
+        continue
+    fi
     raw_dir=$S3_DIR${bioproject}raw/
     n_1=$(aws s3 ls $S3_DIR${bioproject}raw/ | grep "_1.fastq" | wc -l)
     if [ $n_1 == 0 ]; then

commit 014ab293752128ad01b71c6d8584f38cad751000

Fix typo in collect-n-reads.sh introduced in 2e34c208

collect-n-reads.sh
@@ -12,7 +12,7 @@ else
 fi
 
 for bioproject in $(aws s3 ls $S3_DIR | awk '{print $NF}'); do
-    if [ ! -d ${bioproject}metadata/ ] ; then
+    if [ ! -d bioprojects/${bioproject}metadata/ ] ; then
         continue
     fi
     raw_dir=$S3_DIR${bioproject}raw/