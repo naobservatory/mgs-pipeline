0ea674374ca87e9199451e20fe87eb1604a77076
Jeff Kaufman <jeff@securebio.org>
2024-04-15 13:54:38 +0000

    dashboard: separate metadata generation from data processing
    Data processing is very slow, but unlikely to fail.  Metadata generation is
    much faster, and usually fails a few times when you add a new bioproject
    because you haven't got the metadata classifier quite right yet.  Pull these
    into two separate scripts, and move metadata generation as early as possible.
    
    Note that this does mean samples will be included in the sample list even if we
    they haven't finished processing, but I think this is fine.

---
+++ dashboard/prepare-dashboard-metadata.py
@@ -0,0 +1,178 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import glob
+import gzip
+import json
+import math
+import importlib
+import numpy as np
+from collections import Counter
+from collections import defaultdict
+
+ROOT_DIR, MGS_PIPELINE_DIR = sys.argv[1:]
+DASHBOARD_DIR = ROOT_DIR + "/dashboard/"
+
+sys.path.insert(0, DASHBOARD_DIR)
+import sample_metadata_classifier
+
+# bioproject -> [samples]
+bioprojects = defaultdict(set)
+
+# project -> sample -> n_reads
+project_sample_reads = defaultdict(Counter)
+div_samples = defaultdict(set) # sample -> div_samples
+div_sample_reads = {} # div_sample -> n_reads
+for metadata_fname in glob.glob(
+    "%s/bioprojects/*/metadata/metadata.tsv" % ROOT_DIR
+):
+    project = metadata_fname.split("/")[-3]
+    if project in ["PRJEB30546", "PRJNA691135"]:
+        # didn't finish importing this one, and the dashboard chokes on papers
+        # where we don't understand the metadata.
+        continue
+    with open(metadata_fname) as inf:
+        for line in inf:
+            div_sample = line.strip().split("\t")[0]
+            sample = sample_metadata_classifier.recombine(div_sample, project)
+            div_samples[sample].add(div_sample)
+            bioprojects[project].add(sample)
+
+            reads_fname = "%s/bioprojects/%s/metadata/%s.n_reads" % (
+                ROOT_DIR,
+                project,
+                div_sample,
+            )
+            if not os.path.exists(reads_fname):
+                continue
+            with open(reads_fname) as readsf:
+                reads_str = readsf.read().strip()
+                if not reads_str:
+                    continue
+                n_reads = int(reads_str)
+
+                project_sample_reads[project][sample] += n_reads
+                div_sample_reads[div_sample] = n_reads
+
+projects = list(sorted(project_sample_reads))
+
+papers = {}
+for project in projects:
+    with open(
+        "%s/bioprojects/%s/metadata/name.txt" % (ROOT_DIR, project)
+    ) as inf:
+        paper_name = inf.read().strip()
+        if paper_name not in papers:
+            papers[paper_name] = {}
+        if "projects" not in papers[paper_name]:
+            papers[paper_name]["projects"] = []
+        papers[paper_name]["projects"].append(project)
+
+for paper_name in papers:
+    paper_dir = os.path.join(ROOT_DIR, "papers", paper_name.replace(" ", ""))
+    link_fname = os.path.join(paper_dir, "link.txt")
+    if os.path.exists(link_fname):
+        with open(link_fname) as inf:
+            papers[paper_name]["link"] = inf.read().strip()
+
+    na_fname = os.path.join(paper_dir, "na_type.txt")
+    if os.path.exists(na_fname):
+        with open(na_fname) as inf:
+            papers[paper_name]["na_type"] = inf.read().strip()
+
+    subset_fname = os.path.join(paper_dir, "subset.txt")
+    if os.path.exists(subset_fname):
+        with open(subset_fname) as inf:
+            papers[paper_name]["subset"] = inf.read().strip()
+
+# sample -> {metadata}
+sample_metadata = defaultdict(dict)
+
+def summarize_readlength_category(rl_category):
+    weighted_sum = 0
+    weighted_total = 0
+    nc = 0
+    for value, count in rl_category.items():
+        if value == "NC":
+            nc = count
+        else:
+            weighted_sum += int(value)
+            weighted_total += count
+    return [nc,
+            -1 if not weighted_total else weighted_sum / weighted_total]
+
+def summarize_readlengths(rl):
+    return {category: summarize_readlength_category(rl[category])
+            for category in rl}
+
+for project in projects:
+    with open(
+        "%s/bioprojects/%s/metadata/metadata.tsv" % (ROOT_DIR, project)
+    ) as inf:
+        for line in inf:
+            if not line.strip():
+                continue
+            line = line[:-1]  # drop trailing newline
+
+            (
+                div_sample,
+                sample_metadata_dict,
+            ) = sample_metadata_classifier.interpret(
+                project, papers, line.split("\t")
+            )
+            sample = sample_metadata_classifier.recombine(div_sample, project)
+            if sample not in sample_metadata:
+                sample_metadata[sample] = sample_metadata_dict
+            else:
+                assert sample_metadata_dict == sample_metadata[sample]
+
+
+    for sample in project_sample_reads[project]:
+        sample_metadata[sample]["reads"] = project_sample_reads[project][
+            sample
+        ]
+        for div_sample in div_samples[sample]:
+            rl_fname = "readlengths/%s.rl.json.gz" % div_sample
+            try:
+                with gzip.open(rl_fname, "rt") as inf:
+                    rl = json.load(inf)
+            except FileNotFoundError:
+                continue
+
+            sample_metadata[sample]["readlengths"] = summarize_readlengths(rl)
+
+        ribofracs = []
+        weights = []
+        for div_sample in div_samples[sample]:
+            rf_fname = "ribofrac/%s.ribofrac.txt" % div_sample
+            try:
+                with open(rf_fname, "r") as file:
+                    ribofrac = file.readline()
+            except FileNotFoundError:
+                continue
+            ribofracs.append(float(ribofrac))
+            weights.append(div_sample_reads[div_sample])
+
+        if sum(weights):
+            ribofrac = np.average(ribofracs, weights=weights)
+            if not math.isnan(ribofrac):
+                sample_metadata[sample]["ribofrac"] = ribofrac
+
+
+# make it json-serializable
+for bioproject in bioprojects:
+    bioprojects[bioproject] = list(sorted(bioprojects[bioproject]))
+
+for name, val in [
+    ("metadata_samples", sample_metadata),
+    ("metadata_bioprojects", bioprojects),
+    ("metadata_papers", papers),
+]:
+    with open(DASHBOARD_DIR + name + ".json", "w") as outf:
+        json.dump(
+            val,
+            outf,
+            sort_keys=True,
+            indent=2,
+        )
