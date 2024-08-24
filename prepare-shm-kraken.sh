471b0b309667aaef4777a802146e4bfdc81e4a99
Jeff Kaufman <jeff@securebio.org>
2024-08-24 18:52:17 +0000

    prepare-shm-kraken: check for enough memory

--- prepare-shm-kraken.sh
+++ prepare-shm-kraken.sh
@@ -8,6 +8,15 @@ if [ -d /dev/shm/kraken-db/ ]; then
     exit 0
 fi
 
+MEMORY_SIZE=$(cat /proc/meminfo | \
+                  grep MemTotal | \
+                  awk '{print  $2}')
+
+if [[ $MEMORY_SIZE -lt 128000000 ]]; then
+    echo "Insufficient memory; need c6a.16xlarge or bigger"
+    exit 1
+fi
+
 mkdir /dev/shm/kraken-db/
 aws s3 cp s3://genome-idx/kraken/k2_standard_20240605.tar.gz - | \
-    tar -xzv -C /dev/shm/kraken-db
+    tar -xzvv -C /dev/shm/kraken-db
